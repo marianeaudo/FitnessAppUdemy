46c45ba0e6478ff24aa9de028c8150081499e4a6
marianeaudo <marianeaudo@gmail.com>
2020-04-20 17:01:05 +0200

    login reactiveform

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email' : new FormControl(null, [Validators.email, Validators.required]),
+      'password' : new FormControl(null, [Validators.minLength(6), Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
   }
 
 }
