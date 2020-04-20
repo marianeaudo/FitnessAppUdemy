ae24b93d82e89e97c4a2d3651e33b9aee8f4f5c5
marianeaudo <marianeaudo@gmail.com>
2020-04-20 16:17:37 +0200

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+})
+export class MaterialModule {
+
+}
